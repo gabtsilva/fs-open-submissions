c90738abf726e291cdd20809ee080ba73945c807
Gabriel Torres <gabriel.torres.silva@hotmail.com>
2023-12-23 11:41:19 +0100

    Exercises 2.12 to 2.15 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import PersonsList from './components/PersonsList';
+import personService from './services/persons';
 
 const App = () => {
     const [persons, setPersons] = useState([])
@@ -10,21 +11,37 @@ const App = () => {
     const [searchFilter, setSearchFilter] = useState('')
 
     useEffect(() => {
-        fetch('http://localhost:3001/persons')
-            .then(response => response.json())
-            .then(data => setPersons(data))
+        personService.getAll().then(initialPersons => {
+            setPersons(initialPersons)
+        })
     }, []);
+
+    const deletePerson = (name) => {
+        const person = persons.find(person => person.name === name)
+        if (window.confirm(`Delete ${person.name}?`)) {
+            personService.deletePerson(person.id).then((newArray) => {
+                setPersons(newArray);
+            })
+        }
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault()
         const personExists = persons.find(person => person.name === newName)
         if (personExists) {
-            alert(`${newName} is already added to phonebook`)
-            return
+            if (window.confirm(`${personExists.name} is already added to phonebook, replace the old number with a new one?`)) {
+                personService.update(personExists.id, { name: newName, number: newNumber }).then((newPerson) => {
+                    setPersons(persons.map(person => person.id !== newPerson.id ? person : newPerson));
+                })
+            }
+
+        }else{
+            personService.create({ name: newName, number: newNumber }).then(newPerson => {
+                setPersons(persons.concat(newPerson))
+                setNewName('')
+                setNewNumber('')
+            })
         }
-        setPersons(persons.concat({ name: newName, number: newNumber }))
-        setNewName('')
-        setNewNumber('')
     }
 
     return (
@@ -34,7 +51,7 @@ const App = () => {
             <h2>Add a new number</h2>
             <Form handleSubmit={handleSubmit} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
             <h2>Numbers</h2>
-            <PersonsList persons={persons} searchFilter={searchFilter} />
+            <PersonsList persons={persons} searchFilter={searchFilter} deletePerson={deletePerson} />
         </div>
     )
 }
--- part2/phonebook/src/components/PersonsList.jsx
+++ part2/phonebook/src/components/PersonsList.jsx
@@ -1,7 +1,7 @@
-const PersonsList = ({persons, searchFilter}) => {
+const PersonsList = ({persons, searchFilter, deletePerson}) => {
     return(
         <div>
-            {persons.filter(person => person.name.includes(searchFilter)).map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+            {persons.filter(person => person.name.includes(searchFilter)).map(person => <div key={person.name}>{person.name} {person.number} <button onClick={() => deletePerson(person.name)}>delete</button></div>)}
         </div>
     )
 }
